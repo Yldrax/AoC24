4cdc022f70296f51509473b93f74d642420ba91e
Erik Schulz <eschulz2100@gmail.com>
2024-12-03 15:35:08 +0100

    Getting Ready for Day 4

---
+++ 04.py
@@ -0,0 +1,29 @@
+"""
+    December 01, 2024
+
+"""
+
+"""
+    Part 01
+"""
+
+def placeholder1(string1):
+    pass
+
+
+"""
+    Part 02
+"""
+
+def placeholder2(string1):
+    pass
+
+"""
+    Using the Functions
+"""
+
+if __name__ == '__main__':
+    # Getting the String
+    file = open('inputs/04.txt', 'r')
+    input_str = file.read()
+    file.close()
