5ff4e3a922ec0fff42d6d728550e2a13f0441819
Erik Schulz <eschulz2100@gmail.com>
2024-12-04 11:06:39 +0100

    Day 5 preparation and Template file

---
+++ 05.py
@@ -0,0 +1,29 @@
+"""
+    December 05, 2024
+
+"""
+
+"""
+    Part 01
+"""
+
+def placeholder1()
+    pass
+
+"""
+    Part 02
+"""
+
+def placeholder2()
+    pass
+
+"""
+    Using the Functions
+"""
+
+if __name__ == '__main__':
+
+    # Getting the String and splitting it into a list
+    file = open('inputs/05.txt', 'r')
+    input_str = file.read()
+    file.close()
---
+++ xx.py
@@ -0,0 +1,29 @@
+"""
+    December XX, 2024
+
+"""
+
+"""
+    Part 01
+"""
+
+def placeholder1()
+    pass
+
+"""
+    Part 02
+"""
+
+def placeholder2()
+    pass
+
+"""
+    Using the Functions
+"""
+
+if __name__ == '__main__':
+
+    # Getting the String and splitting it into a list
+    file = open('inputs/xx.txt', 'r')
+    input_str = file.read()
+    file.close()
